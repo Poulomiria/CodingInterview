507a97354cab8cff1510e129be16cdc0e2548417
Poulomi Roy Chowdhury <poulomi.juit@gmail.com>
2022-07-20 14:36:23 +0530

    Coding Pattern

---
+++ CodingPattern/src/FastAndSlowPointers/LinkedListCycle.java
@@ -0,0 +1,52 @@
+package FastAndSlowPointers;
+
+
+class ListNode {
+	  int value = 0;
+	  ListNode next;
+
+	  ListNode(int value) {
+	    this.value = value;
+	  }
+	}
+
+	class LinkedListCycle {
+
+	  public static boolean hasCycle(ListNode head) {
+	    ListNode slow = head;
+	    ListNode fast = head;
+	    while (fast != null && fast.next != null) {
+	      fast = fast.next.next;
+	      slow = slow.next;
+	      if (slow == fast)
+	        return true; // found the cycle
+	      //return calculateLength(slow);// calculate the length
+	    }
+	    return false;
+	  }
+	  private static int calculateLength(ListNode slow) {
+		    ListNode current = slow;
+		    int cycleLength = 0;
+		    do {
+		      current = current.next;
+		      cycleLength++;
+		    } while (current != slow);
+		    return cycleLength;
+		  }
+
+	  public static void main(String[] args) {
+	    ListNode head = new ListNode(1);
+	    head.next = new ListNode(2);
+	    head.next.next = new ListNode(3);
+	    head.next.next.next = new ListNode(4);
+	    head.next.next.next.next = new ListNode(5);
+	    head.next.next.next.next.next = new ListNode(6);
+	    System.out.println("LinkedList has cycle: " + LinkedListCycle.hasCycle(head));
+
+	    head.next.next.next.next.next.next = head.next.next;
+	    System.out.println("LinkedList has cycle: " + LinkedListCycle.hasCycle(head));
+
+	    head.next.next.next.next.next.next = head.next.next.next;
+	    System.out.println("LinkedList has cycle: " + LinkedListCycle.hasCycle(head));
+	  }
+	}
---
+++ LeetCodeSolve/src/Medium/NumberOfMatchingSubsequences.java
@@ -0,0 +1,60 @@
+package Medium;
+/*
+ * https://leetcode.com/problems/number-of-matching-subsequences/
+ */
+
+import java.util.ArrayList;
+
+class NumberOfMatchingSubsequences {
+	
+	public int numMatchingSubseqDifferent(String S, String[] words) {
+	    List<Integer[]>[] waiting = new List[128];
+	    for (int c = 0; c <= 'z'; c++)
+	        waiting[c] = new ArrayList();
+	    for (int i = 0; i < words.length; i++)
+	        waiting[words[i].charAt(0)].add(new Integer[]{i, 1});
+	    for (char c : S.toCharArray()) {
+	        List<Integer[]> advance = new ArrayList(waiting[c]);
+	        waiting[c].clear();
+	        for (Integer[] a : advance)
+	            waiting[a[1] < words[a[0]].length() ? words[a[0]].charAt(a[1]++) : 0].add(a);
+	    }
+	    return waiting[0].size();
+	}
+	
+    public int numMatchingSubseq(String S, String[] words) {
+        int ans = 0;
+        ArrayList<Node>[] heads = new ArrayList[26];
+        for (int i = 0; i < 26; ++i)
+            heads[i] = new ArrayList<Node>();
+
+        for (String word: words)
+            heads[word.charAt(0) - 'a'].add(new Node(word, 0));
+
+        for (char c: S.toCharArray()) {
+            ArrayList<Node> old_bucket = heads[c - 'a'];
+            heads[c - 'a'] = new ArrayList<Node>();
+
+            for (Node node: old_bucket) {
+                node.index++;
+                if (node.index == node.word.length()) {
+                    ans++;
+                } else {
+                    heads[node.word.charAt(node.index) - 'a'].add(node);
+                }
+            }
+            old_bucket.clear();
+        }
+        return ans;
+    }
+
+}
+
+class Node {
+    String word;
+    int index;
+    public Node(String w, int i) {
+        word = w;
+        index = i;
+    }
+}
